4119125a50ad0e6196e8fd1f9abae996ac560bb4
Dayron-z <zbastian.zapata@gmail.com>
2024-01-27 16:21:36 -0500

    Commit importante1 (El db.json se llena correctamente, el e.preventDefault no funciona correctamente

--- js/login.js
+++ js/login.js
@@ -5,11 +5,13 @@ const sign_in_btn2 = document.querySelector("#sign-in-btn2");
 const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 
 
-sign_up_btn.addEventListener("click",() => {
+sign_up_btn.addEventListener("click",(e) => {
+  e.preventDefault()
   container.classList.toggle("sign-up-mode");
 });
 
-sign_in_btn.addEventListener("click",() => {
+sign_in_btn.addEventListener("click",(e) => {
+  e.preventDefault()
   container.classList.toggle("sign-up-mode");
 });
 
@@ -24,15 +26,58 @@ container.classList.toggle("sign-in-mode2");
 });
 
 
-//Json server.
-const formularioRegistro = document.querySelector(".sign-up-form")
 
-formularioRegistro.addEventListener("submit", (e) => {
+
+
+
+
+//Formularios
+
+
+const formularioRegistro = document.querySelector(".sign-up-form")
+const formularioLogin = document.querySelector(".sign-in-form")
+
+formularioRegistro.addEventListener("submit", async (e) => {
   e.preventDefault();
-  obtenerData();
+  await obtenerDataRegistro();
+  validarLogin();
 })
 
 
-function obtenerData() {
-  
+async function obtenerDataRegistro() {
+  const user = document.getElementById("sign-up-user").value;
+  const password = document.getElementById("sign-up-password").value;
+
+  const URL = "http://localhost:3000/users";
+
+  try {
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user, password })
+    });
+
+    if (response.ok) {
+      alert("wepaaa")
+      console.log("Usuario registrado exitosamente");
+      // Puedes realizar acciones adicionales aquí después de un registro exitoso.
+    } else {
+      console.error("Error al registrar el usuario");
+      // Puedes manejar errores aquí, por ejemplo, mostrar un mensaje al usuario.
+    }
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+    // Puedes manejar errores de red u otros aquí.
+  }
 }
+
+
+
+
+
+
+
+
+
