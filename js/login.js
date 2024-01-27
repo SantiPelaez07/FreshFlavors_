a218ea845e05b72aee6158e6347ba699e3c9fbe2
Dayron-z <zbastian.zapata@gmail.com>
2024-01-26 19:34:18 -0500

    Login and register finished

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container")
+const sign_in_btn2 = document.querySelector("#sign-in-btn2");
+const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 
 
 sign_up_btn.addEventListener("click",() => {
@@ -11,4 +13,16 @@ sign_up_btn.addEventListener("click",() => {
 sign_in_btn.addEventListener("click",() => {
     console.log("hice click");
   container.classList.toggle("sign-up-mode");
+});
+
+sign_up_btn2.addEventListener("click",(e) => {
+  e.preventDefault()
+  console.log("hice click");
+container.classList.toggle("sign-up-mode2");
+});
+
+sign_in_btn2.addEventListener("click",(e) => {
+  e.preventDefault()
+  console.log("hice click");
+container.classList.toggle("sign-in-mode2");
 });
