1821cddf34a15add6f988d05c1ea454ce383b796
SantiPelaez07 <102003216+SantiPelaez07@users.noreply.github.com>
2024-02-05 12:11:21 -0500

    Arreglo en login y ajuste de img

--- js/incio-logueado.js
+++ js/incio-logueado.js
@@ -2,9 +2,9 @@
 //Funcionalidad de la página de inicio del usuario logueado
 //Selectores
 const title = document.querySelector(".title");
-const exit = document.getElementById("exit");
-const option = document.querySelector("#salir")
+const exit = document.querySelector("#exit");
 const fristContainer = document.querySelector(".frist-container1")
+const userLogin = document.querySelector(".dropbtn")
 const URL = "http://localhost:3000/users"
 
 
@@ -13,6 +13,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
   event.preventDefault();
   const localUser = localStorage.getItem("datas")
   const listLocal = JSON.parse(localUser)
+  userLogin.innerHTML = `${listLocal[0].user}`
+  getUser()
 
   const list = {
     id: listLocal.id,
@@ -21,32 +23,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
     password: localUser
 }
 
-  console.log(`Contraseña: ${localUser  }`)
-  // listLocal = JSON.parse(localUser)
-  // console.log(listLocal);
-  getUser()
-  // selectPerfil();
-  
-// exit.addEventListener("click", (event) => {
-//        event.preventDefault();
-//        cerraSesion();
-//      })
-     
+  console.log(`Contraseña: ${localUser}`)
 
     function getUser() {
       title.innerHTML = ""
-      title.innerHTML = `¡Bienvenido ${localUser[0].user}, estamos ansiosos de verte usando nuestros servicios!`
+      title.innerHTML = `¡Bienvenido ${listLocal[0].user}, estamos ansiosos de verte usando nuestros servicios!`
     }
   })
 
-// let i, name
-// for (i=0; i <= data1.length; i++) {
-
-//   if (data1[i] == data) {
-//     name = data1[i]
-//   }
+  exit.addEventListener("click", (event) => {
+    event.preventDefault();
+    cerraSesion();
+  })
 
 function cerraSesion() {
-  localStorage.removeItem('token');
+  localStorage.removeItem("datas");
   window.location.href = '/index.html';
 }
